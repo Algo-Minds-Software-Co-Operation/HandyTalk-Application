5171cf38abd7edd7285d58a80766757d77154cbd
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-06-23 16:46:28 +0530

    title and description added.

--- lib/signup_username_screen.dart
+++ lib/signup_username_screen.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
 
-class SignupUsernameScreen extends StatelessWidget{
+class SignupUsernameScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            'assets/images/background.png',
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 470.0),
+              child: Text(
+                'Enter Your Username',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 415.0),
+              child: Text(
+                'Enter a new username for login',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+                padding: const EdgeInsets.only(bottom: 355.0),
+                child: Image.asset(
+                  'assets/images/page-circle-one.png',
+                  width: 50,
+                  height: 50,
+                )),
+          ),
+          Center(
+            child: Padding(
+                padding: const EdgeInsets.only(top: 450.0),
+                child: Image.asset(
+                  'assets/images/3d-model.png',
+                  width: 400,
+                  height: 400,
+                )),
+          ),
+        ],
+      ),
+    );
   }
 
 }
