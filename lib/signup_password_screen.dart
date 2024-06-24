fb6c3b9c9c507d3ca9a5f5a79143adf1f9f673ec
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-06-24 10:58:34 +0530

    title and desc added.

--- lib/signup_password_screen.dart
+++ lib/signup_password_screen.dart
@@ -3,8 +3,108 @@ import 'package:flutter/material.dart';
 class SignupPasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            'assets/images/background.png',
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 470.0),
+              child: Text(
+                'Enter New Password',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 415.0),
+              child: Text(
+                'Enter a new password',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+                padding: const EdgeInsets.only(bottom: 355.0),
+                child: Image.asset(
+                  'assets/images/page-circle-one.png',
+                  width: 50,
+                  height: 50,
+                )),
+          ),
+          Center(
+            child: Padding(
+                padding: const EdgeInsets.only(top: 450.0),
+                child: Image.asset(
+                  'assets/images/3d-model.png',
+                  width: 400,
+                  height: 400,
+                )),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person_outline),
+                          hintText: 'New Password',
+                          filled: true,
+                          fillColor: Colors.transparent,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(4.0),
+                          ),
+                        ),
+                      )),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle the next button press
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 11, 133, 240),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                    ),
+                    child: Text(
+                      'Continue',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Roboto',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
-  
 }
