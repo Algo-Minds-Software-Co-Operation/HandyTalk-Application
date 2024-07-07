465110f09a8d2670dcda72ee803d9c0f84a80436
kavindumal <kavindu11250403@gmail.com>
2024-07-07 22:59:51 +0530

    add firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,21 @@ import 'package:HandyTalk/edit_model.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user == null) {
+      print('User is currently signed out!');
+    } else {
+      print('User is signed in!');
+    }
+  });
   runApp(MyApp());
 }
 
